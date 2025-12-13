04eeb55c53b4f1c6c0261b192a4f0109341ae2dc
Fadibatarseh <MRBATARSEH@GMAIL.COM>
2025-12-13 12:41:13 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FamilyTreeApp from "./FamilyTreeApp";
+import FamilyTreeApp from "./FamilyTreeApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
